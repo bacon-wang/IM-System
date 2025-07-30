9cf2decfc27c29ff2a50fcf09de1b85fddf5a18f
王培根 <sidiupide233@icloud.com>
2025-07-31 02:27:07 +0800

    用策略模式重构 DoMessage

--- user.go
+++ user.go
@@ -73,52 +73,131 @@ func (u *User) SendMsg(msg string) {
 	u.conn.Write([]byte(msg))
 }
 
-// buissness logic
+// MessageType represents different types of messages
+type MessageType int
+
+const (
+	MessageTypeWho MessageType = iota
+	MessageTypeRename
+	MessageTypePrivate
+	MessageTypeBroadcast
+)
+
+// MessageHandler defines the interface for message handlers
+type MessageHandler interface {
+	Handle(u *User, msg string) bool
+}
+
+// WhoHandler handles "who" command
+type WhoHandler struct{}
+
+func (h *WhoHandler) Handle(u *User, msg string) bool {
+	if msg != "who" {
+		return false
+	}
+
+	who := "online users: ["
+	u.server.OnlineUsersLock.RLock()
+	for username := range u.server.OnlineUsers {
+		who += username + ", "
+	}
+	u.server.OnlineUsersLock.RUnlock()
+
+	if len(who) > len("online users: [") {
+		who = who[:len(who)-2]
+	}
+	who += "]\n"
+
+	u.SendMsg(who)
+	return true
+}
+
+// RenameHandler handles "rename" command
+type RenameHandler struct{}
+
+func (h *RenameHandler) Handle(u *User, msg string) bool {
+	if len(msg) <= 7 || msg[:7] != "rename " {
+		return false
+	}
+
+	newName := msg[7:]
+	if u.server.isOnline(newName) {
+		u.SendMsg("User name already exists\n")
+		return true
+	}
+
+	u.ModifyName(newName)
+	return true
+}
+
+// PrivateHandler handles private messages
+type PrivateHandler struct{}
+
+func (h *PrivateHandler) Handle(u *User, msg string) bool {
+	if !strings.HasPrefix(msg, "to ") || len(msg) <= 3 {
+		return false
+	}
+
+	split := strings.Split(msg, " ")
+	if len(split) < 3 {
+		u.SendMsg("Invalid message format, usage: to {username} {msg}\n")
+		return true
+	}
+
+	username := split[1]
+	if !u.server.isOnline(username) {
+		u.SendMsg("User not online\n")
+		return true
+	}
+
+	content := strings.Join(split[2:], " ")
+	u.server.OnlineUsers[username].SendMsg(u.Name + " said to you: " + content + "\n")
+	return true
+}
+
+// BroadcastHandler handles broadcast messages
+type BroadcastHandler struct{}
+
+func (h *BroadcastHandler) Handle(u *User, msg string) bool {
+	u.server.broadCast(u, msg)
+	return true
+}
+
+// MessageProcessor processes messages using registered handlers
+type MessageProcessor struct {
+	handlers []MessageHandler
+}
+
+// NewMessageProcessor creates a new message processor with default handlers
+func NewMessageProcessor() *MessageProcessor {
+	return &MessageProcessor{
+		handlers: []MessageHandler{
+			&WhoHandler{},
+			&RenameHandler{},
+			&PrivateHandler{},
+			&BroadcastHandler{}, // This should be last as it always returns true
+		},
+	}
+}
+
+// Process processes a message using the registered handlers
+func (mp *MessageProcessor) Process(u *User, msg string) {
+	for _, handler := range mp.handlers {
+		if handler.Handle(u, msg) {
+			return
+		}
+	}
+}
+
+// business logic
 func (u *User) DoMessage(msg string) {
 	u.server.Logger.Debug("recv from user",
 		zap.String("name", u.Name),
 		zap.String("message", msg))
 
-	if msg == "who" { // who command
-		who := "online users: ["
-
-		u.server.OnlineUsersLock.RLock()
-		for username := range u.server.OnlineUsers {
-			who += username + ", "
-		}
-		u.server.OnlineUsersLock.RUnlock()
-
-		who = who[:len(who)-2]
-		who += "]\n"
-
-		u.SendMsg(who)
-	} else if len(msg) > 7 && msg[:7] == "rename " {
-		// rename {new name}
-		newName := msg[7:]
-		if u.server.isOnline(newName) {
-			u.SendMsg("User name already exists\n")
-			return
-		}
-
-		u.ModifyName(newName)
-	} else if strings.HasPrefix(msg, "to ") && len(msg) > 3 {
-		// to {username} {msg}
-		split := strings.Split(msg, " ")
-		if len(split) < 3 {
-			u.SendMsg("Invalid message format, useage: to {username} {msg}\n")
-			return
-		}
-
-		username := split[1]
-		if !u.server.isOnline(username) {
-			u.SendMsg("User not online\n")
-			return
-		}
-		content := split[2]
-		u.server.OnlineUsers[username].SendMsg(u.Name + " said to you:" + content + "\n")
-	} else { // normal data
-		u.server.broadCast(u, msg)
-	}
+	// Use message processor to handle the message
+	processor := NewMessageProcessor()
+	processor.Process(u, msg)
 }
 
 // modify username
